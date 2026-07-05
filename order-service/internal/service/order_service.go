b7706e98a9f235d9ae2c2de7f73dd986e9d6004e
Ororura <hitmanlol890@gmail.com>
2026-07-05 12:49:49 +0300

    create order service

---
+++ order-service/internal/service/order_service.go
@@ -0,0 +1,61 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"order-service/internal/kafka"
+	"order-service/internal/model"
+	"order-service/internal/repository"
+)
+
+type OrderService struct {
+	repo     *repository.OrderRepository
+	producer *kafka.Producer
+}
+
+func NewOrderService(repo *repository.OrderRepository, producer *kafka.Producer) *OrderService {
+	return &OrderService{
+		repo, producer,
+	}
+}
+
+func (s *OrderService) CreateOrder(ctx context.Context, req model.CreateOrderRequest) (model.Order, error) {
+	if req.UserID <= 0 {
+		return model.Order{}, errors.New("user_id is required")
+	}
+
+	if req.ProductID <= 0 {
+		return model.Order{}, errors.New("product_id is required")
+	}
+
+	if req.Count <= 0 {
+		return model.Order{}, errors.New("count must be greater than zero")
+	}
+
+	order := model.Order{
+		UserID:    req.UserID,
+		ProductID: req.ProductID,
+		Count:     int(req.Count),
+		Status:    "created",
+	}
+
+	created, err := s.repo.Create(ctx, order)
+	if err != nil {
+		return model.Order{}, err
+	}
+
+	event := kafka.OrderCreatedEvent{
+		OrderID:   created.ID,
+		UserID:    created.UserID,
+		ProductID: created.ProductID,
+		Count:     created.Count,
+		Status:    created.Status,
+		CreatedAt: created.CreatedAt,
+	}
+
+	if err := s.producer.PublishOrderCreated(ctx, event); err != nil {
+		return model.Order{}, err
+	}
+
+	return created, nil
+}
