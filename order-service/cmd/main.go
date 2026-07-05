659973e63c172737d42721cca203a695b26b9f75
Ororura <hitmanlol890@gmail.com>
2026-07-05 13:13:31 +0300

    init main.go

--- order-service/cmd/main.go
+++ order-service/cmd/main.go
@@ -1,5 +1,77 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
+	"order-service/internal/config"
+	"order-service/internal/database"
+	"order-service/internal/handler"
+	"order-service/internal/kafka"
+	"order-service/internal/repository"
+	"order-service/internal/service"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func main() {
+	cfg := config.MustLoad()
+
+	db, err := database.NewPostgres(cfg.DBURL)
+	if err != nil {
+		log.Fatal("failed to connect postgres:", err)
+	}
+	defer db.Close()
+
+	producer := kafka.NewProducer(cfg.KafkaBrokets, cfg.KafkaOrderCreatedTopic)
+	defer producer.Close()
+
+	orderRepo := repository.NewOrderRepository(db)
+	orderService := service.NewOrderService(orderRepo, producer)
+	orderHandler := handler.NewOrderHandler(orderService)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/orders", orderHandler.CreateOrder)
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	server := &http.Server{
+		Addr:         ":" + cfg.HTTPPort,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Println("order-service started on port", cfg.HTTPPort)
+
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal("server error:", err)
+		}
+	}()
+
+	waitFotShutDown(server)
+}
+
+func waitFotShutDown(server *http.Server) {
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+	log.Println("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println("server shutdown error:", err)
+	}
+
+	log.Println("server stopped")
 }
